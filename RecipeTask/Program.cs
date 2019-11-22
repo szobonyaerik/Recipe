4c7b1a72fa74719af198a307d0338f118211854a
Erik Szobonya <szobonyaerik@gmail.com>
2019-11-22 14:10:26 +0100

    recipe done

--- RecipeTask/Program.cs
+++ RecipeTask/Program.cs
@@ -10,6 +10,40 @@ namespace RecipeTask
     {
         static void Main(string[] args)
         {
+            Recipe rantotthus = new Recipe(1111, "Rántotthús", new List<string>() { "Csirke", "Tojás", "Zsemlemorzsa" });
+            Recipe rantotthusCopy = (Recipe)rantotthus.DeepCopy();
+            rantotthusCopy.Ingredient.Add("Sajt");
+
+            Recipe rizsesHal = new Recipe(1112, "Rizses Hal", new List<string>() { "Hal", "Rizs", });
+            Recipe rizsesHalCopy = (Recipe)rizsesHal.DeepCopy();
+            rizsesHalCopy.ID = 1110;
+
+            Recipe lecso = new Recipe(1113, "Lecsó", new List<string>() { "Paprika", "Paradicsom","Tojás", "Kolbász" });
+            Recipe lecsoCopy = (Recipe)lecso.DeepCopy();
+            lecsoCopy.name = "Tojásos Lecsó";
+
+            Recipe bundasKenyer = new Recipe(1114, "Bundáskenyér", new List<string>() { "Kenyér", "Tojás", });
+
+            Recipe spaghetti = new Recipe(1115, "Spaghetti", new List<string>() { "Tészta", "Paradicsom","Sajt","Darálthús" });
+
+
+            Console.WriteLine(rantotthus.ToString());
+            Console.WriteLine(rantotthusCopy.ToString());
+            Console.WriteLine();
+
+            Console.WriteLine(rizsesHal.ToString());
+            Console.WriteLine(rizsesHalCopy.ToString());
+            Console.WriteLine();
+
+            Console.WriteLine(lecso.ToString());
+            Console.WriteLine(lecsoCopy.ToString());
+            Console.WriteLine();
+
+            Console.WriteLine(bundasKenyer.ToString());
+            Console.WriteLine();
+            Console.WriteLine(spaghetti.ToString());
+
+            Console.ReadLine();
         }
     }
 }
---
+++ RecipeTask/Recipe.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeTask
+{
+    class Recipe
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public List<string> Ingredient { get; set; }
+
+        public Recipe(int ID, string name, List<string> Ingredient)
+        {
+            this.ID = ID;
+            this.name = name;
+            this.Ingredient = Ingredient;
+
+        }
+
+        public override string ToString()
+        {
+            string Ingredient = String.Join(",", this.Ingredient);
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{this.ID}, {this.name}, {Ingredient}");
+            return sb.ToString();
+            
+        }
+
+        public Recipe DeepCopy()
+        {
+            List<string> CopiedIngredients = new List<string>();
+
+            foreach (var element in this.Ingredient)
+            {
+                CopiedIngredients.Add(element);
+            }
+
+            Recipe DeepCopyRecipe = new Recipe(this.ID, this.name, CopiedIngredients);
+
+            return DeepCopyRecipe;
+
+
+                       
+        }
+
+    }
+}
